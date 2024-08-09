6b07d97176bacf8b65d6d49d5ae89e3bb543bf64
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2024-08-09 16:47:49 +0200

    fix: update scripts to use new /etc path

--- files/scripts/sddm/disable-sddm-virtualkbd.sh
+++ files/scripts/sddm/disable-sddm-virtualkbd.sh
@@ -3,4 +3,4 @@ set -oue pipefail
 
 # Remove virtual keyboard on SDDM
 # as this image is for workstations and permanently docked steamdecks
-rm /usr/etc/sddm.conf.d/virtualkbd.conf
+rm /etc/sddm.conf.d/virtualkbd.conf
--- files/scripts/sddm/enable-sddm.sh
+++ files/scripts/sddm/enable-sddm.sh
@@ -2,7 +2,7 @@
 set -oue pipefail
 
 # Remove the autologin to gamescope-session
-rm /usr/etc/sddm.conf.d/steamos.conf
+rm /etc/sddm.conf.d/steamos.conf
 
 # Undo some bazzite-deck specific services we do not need anymore
 systemctl disable bazzite-autologin.service && \
