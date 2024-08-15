ac531972bc31ab593cf2bd83d2d2af39bcef0f6f
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2024-08-15 02:39:06 +0200

    feat: add klassy window decorations

--- files/scripts/main/deckstation-setup.sh
+++ files/scripts/main/deckstation-setup.sh
@@ -6,3 +6,14 @@ semanage fcontext -a -t svirt_tmpfs_t /dev/shm/looking-glass
 
 IMAGE_REF="ostree-image-signed:docker://$IMAGE_REGISTRY/$IMAGE_NAME"
 printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest",\n"image-name": "'"$IMAGE_NAME"'"\n}' > /usr/share/ublue-os/image-info.json
+
+# Add Klassy repo
+#OS_VERSION=$(grep "VERSION_ID" /etc/os-release | sed 's/VERSION_ID=//')
+curl -sL https://download.opensuse.org/repositories/home:paul4us/Fedora_${OS_VERSION}/home:paul4us.repo | tee /etc/yum.repos.d/home_paul4us.repo
+sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/home_paul4us.repo
+
+# Install Klassy window decorations
+rpm-ostree install klassy
+
+# Disable repository
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/home_paul4us.repo
