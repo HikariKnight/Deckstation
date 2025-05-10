2316016f2fef2aea42066b17343223a99f8ad7a2
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2025-05-10 03:30:48 +0200

    fix: remove ublue-update.timer service line from testing

---
+++ files/scripts/sddm/enable-sddm-testing.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+# Remove the autologin to gamescope-session
+rm /etc/sddm.conf.d/steamos.conf
+
+# Undo some bazzite-deck specific services we do not need anymore
+systemctl disable bazzite-autologin.service
