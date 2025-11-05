5f5cc92c94f7b0badd59e57df90bd0602ad3fecb
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2025-11-05 16:54:56 +0100

    chore: add bash completion for ujust

--- files/scripts/main/add-justfile.sh
+++ files/scripts/main/add-justfile.sh
@@ -3,3 +3,6 @@ set -oue pipefail
 
 # Add deckstation justfile to 60-custom.just
 echo 'import "/usr/share/ublue-os/just/65-deckstation.just"' >> /usr/share/ublue-os/justfile
+
+# Generare bash completion
+just --completions bash | sed -E 's/([\(_" ])just/\1ujust/g' > /usr/share/bash-completion/completions/ujust
