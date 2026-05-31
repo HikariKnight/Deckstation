769623f00dfdb01c21ce015e0274a5995e7f5e11
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2026-05-31 02:25:36 +0200

    feat: prepare for plasmalogin manager?

--- files/scripts/sddm/disable-sddm-virtualkbd.sh
+++ files/scripts/sddm/disable-sddm-virtualkbd.sh
@@ -3,4 +3,9 @@ set -oue pipefail
 
 # Remove virtual keyboard on SDDM
 # as this image is for workstations and permanently docked steamdecks
-rm /etc/sddm.conf.d/virtualkbd.conf
+# Remove the autologin to gamescope-session
+if [ -d "/etc/sddm.conf.d" ]; then
+    rm /etc/sddm.conf.d/virtualkbd.conf
+elif [ -d "/etc/plasmalogin.conf.d" ]; then
+    rm /etc/plasmalogin.conf.d/virtualkbd.conf
+fi
--- files/scripts/sddm/enable-sddm-testing.sh
+++ files/scripts/sddm/enable-sddm-testing.sh
@@ -2,7 +2,11 @@
 set -oue pipefail
 
 # Remove the autologin to gamescope-session
-rm /etc/sddm.conf.d/steamos.conf
+if [ -f "/etc/sddm.conf.d/steamos.conf" ]; then
+    rm /etc/sddm.conf.d/steamos.conf
+elif [ -f "/etc/plasmalogin.conf.d/steamos.conf" ]; then
+    rm /etc/plasmalogin.conf.d/steamos.conf
+fi
 
 # Undo some bazzite-deck specific services we do not need anymore
-systemctl disable bazzite-autologin.service
+#systemctl disable bazzite-autologin.service
