878789356a9805acdf420612fe92afbe3b82cf77
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2026-08-04 21:20:59 +0200

    fix: correctly remove virtualkbd if it exists

--- files/scripts/sddm/disable-sddm-virtualkbd.sh
+++ files/scripts/sddm/disable-sddm-virtualkbd.sh
@@ -4,8 +4,8 @@ set -oue pipefail
 # Remove virtual keyboard on SDDM
 # as this image is for workstations and permanently docked steamdecks
 # Remove the autologin to gamescope-session
-if [ -d "/etc/sddm.conf.d" ]; then
+if [ -f "/etc/sddm.conf.d/virtualkbd.conf" ]; then
     rm /etc/sddm.conf.d/virtualkbd.conf
-elif [ -d "/etc/plasmalogin.conf.d" ]; then
+elif [ -f "/etc/plasmalogin.conf.d/virtualkbd.conf" ]; then
     rm /etc/plasmalogin.conf.d/virtualkbd.conf
 fi
